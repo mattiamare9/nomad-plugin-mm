c12377b23e6c3fe0d498aff44bb9a5edad5c6af6
Mattia Marella <mattia.marella@edu.unife.it>
2025-02-08 16:53:21 +0100

    parser implementation

--- src/nomad_plugin_mm/parsers/parser.py
+++ src/nomad_plugin_mm/parsers/parser.py
@@ -14,6 +14,8 @@ from nomad.config import config
 from nomad.datamodel.metainfo.workflow import Workflow
 from nomad.parsing.parser import MatchingParser
 
+import json
+from nomad_plugin_mm.schema_packages.cams import Run
 configuration = config.get_plugin_entry_point(
     'nomad_plugin_mm.parsers:parser_entry_point'
 )
@@ -29,4 +31,24 @@ class NewParser(MatchingParser):
     ) -> None:
         logger.info('NewParser.parse', parameter=configuration.parameter)
 
-        archive.workflow2 = Workflow(name='test')
+
+        #data_dict = json.load(mainfile)
+        
+        with open(mainfile, 'r') as file:
+            data_dict = json.load(file)
+
+        
+        run = Run()
+        run.run_name = data_dict['run_name']
+        run.project = data_dict['project']
+        run.workpackage = data_dict['workpackage']
+        run.run_description = data_dict['run_description']
+        run.author = data_dict['author']
+        run.cost_model = data_dict['cost_model']
+        run.start_date = data_dict['start_date']
+        run.generated_on = data_dict['generated_on']        
+
+        archive.data = run
+
+        logger.info("Alles Gut")
+
--- src/nomad_plugin_mm/schema_packages/__init__.py
+++ src/nomad_plugin_mm/schema_packages/__init__.py
@@ -15,3 +15,14 @@ schema_package_entry_point = NewSchemaPackageEntryPoint(
     name='NewSchemaPackage',
     description='New schema package entry point configuration.',
 )
+
+class CAMSEntryPoint(SchemaPackageEntryPoint):
+    def load(self):
+        from nomad_plugin_mm.schema_packages.cams import m_package
+
+        return m_package
+    
+cams_entry_point = CAMSEntryPoint(
+    name='CAMSPackage',
+    description='CAMSPackage entry point configuration.',
+)
